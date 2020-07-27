478716e5f8fa2f8ee24fd1b9c88f812a6403607c
Arghya Shubhshiv <57139099+ArghyaShubhshiv@users.noreply.github.com>
2020-07-27 20:38:28 +0530

    Update index.js

--- website/index.js
+++ website/index.js
@@ -1,11 +1,11 @@
 var slideContainer = document.getElementsByTagName("div")[0];
 var condition = document.getElementById("detox_condition");
 
-setInterval(function(){
-  if (condition.textContent == "Without Detox"){
+setInterval(function () {
+  if (condition.textContent == "Without Detox") {
     condition.textContent = "With Detox";
   } else {
     condition.textContent = "Without Detox";
   }
   slideContainer.classList.toggle("slides");
-},2000)
+}, 2000);
