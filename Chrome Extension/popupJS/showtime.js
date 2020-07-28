b9fb125f7aca993c72544c62f08fcd90a3c93026
Arghya Shubhshiv <57139099+ArghyaShubhshiv@users.noreply.github.com>
2020-07-28 14:45:01 +0530

    Update showtime.js

--- Chrome Extension/popupJS/showtime.js
+++ Chrome Extension/popupJS/showtime.js
@@ -4,25 +4,16 @@ var svgContainer = document.getElementsByTagName("svg")[0];
 var circleSVG = document.getElementsByTagName("circle")[0];
 var restartButton = document.querySelector("#restartTimer");
 
-settingsAtStart();
-
 chrome.runtime.onMessage.addListener((message) => {
   //receives the time from background.js and shows it
   if (message.msg == "started") {
     settingsAtStart();
   } else if (message.msg == "break") {
-    restartButton.style.display = "none";
-    settingsAtBreak();
     settingsAtBreak();
   } else {
     showTime.textContent = message.msg;
   }
   console.log(message.msg);
-});
-
-restartButton.addEventListener("click", function () {
-  let message = { msg: "button_clicked" };
-  chrome.runtime.sendMessage(message);
 });
 
 if (localStorage.getItem("is_break") == "true") {
@@ -31,23 +22,28 @@ if (localStorage.getItem("is_break") == "true") {
   settingsAtStart();
 }
 
+restartButton.addEventListener("click", function () {
+  let message = { msg: "button_clicked" };
+  chrome.runtime.sendMessage(message);
+});
+
 function settingsAtStart() {
-  labelTime.style.visibility = "visible";
-  circleSVG.style.display = "initial";
-  restartButton.style.display = "none";
   if (localStorage.getItem("is_break") == "true") {
     localStorage.removeItem("is_break");
   }
   localStorage.setItem("is_break", "false");
+  labelTime.style.visibility = "visible";
+  circleSVG.style.display = "initial";
+  restartButton.style.display = "none";
 }
 
 function settingsAtBreak() {
-  labelTime.style.visibility = "hidden";
-  showTime.textContent = "BREAK!!!";
-  circleSVG.style.display = "none";
-  restartButton.style.display = "initial";
   if (localStorage.getItem("is_break") == "false") {
     localStorage.removeItem("is_break");
   }
   localStorage.setItem("is_break", "true");
+  labelTime.style.visibility = "hidden";
+  showTime.textContent = "BREAK!!!";
+  circleSVG.style.display = "none";
+  restartButton.style.display = "initial";
 }
